c8812fe1fdcfeddda4a4b1aa870e153e20e92c82
aleshark87 <47444510+aleshark87@users.noreply.github.com>
2019-03-13 17:08:52 +0100

    Start

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "libreria.h"
+#include <stdlib.h>
+#include <stdio.h>
+#define LENGTH 17
+
+int main(){
+	char**campo1;
+	campo1=(char **)malloc(sizeof(char*)*LENGTH);
+	if(campo1==NULL)
+		return 1;
+	//frega un cazzo dei controlli
+	for(int i=0;i<17;++i)
+	{
+		*(campo1+i)=(char *)malloc(sizeof(char )*LENGTH);
+	}
+	inizializza_campo(campo1);
+	stampa_campo(campo1);
+	mossa_pedina(campo1,1,0);
+	stampa_campo(campo1);
+	return 0;
+}
+
+	
