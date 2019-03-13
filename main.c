27713129e2720398f67d5f3de3833c1601d1f436
aleshark87 <47444510+aleshark87@users.noreply.github.com>
2019-03-13 18:18:51 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,14 +3,6 @@
 
 int main(){
 	char**campo1;
-	campo1=(char **)malloc(sizeof(char*)*LENGTH);
-	if(campo1==NULL)
-		return 1;
-	//frega un cazzo dei controlli
-	for(int i=0;i<17;++i)
-	{
-		*(campo1+i)=(char *)malloc(sizeof(char )*LENGTH);
-	}
 	inizializza_campo(campo1);
 	stampa_campo(campo1);
 	mossa_pedina(campo1,1,0);
