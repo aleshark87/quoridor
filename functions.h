56dea06db3f22fe8de86d9f71eacfe98492b069a
aleshark87 <47444510+aleshark87@users.noreply.github.com>
2019-03-14 10:14:14 +0100

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,5 +1,4 @@
-#ifndef FUNCTIONS_H_INCLUDED
-#define FUNCTIONS_H_INCLUDED
+
 #include <stdio.h>
 #include <stdlib.h>
 struct player{
@@ -12,4 +11,4 @@ void print_matrix(char **m,player *plr1,player *plr2);
 void start(char **m,player *plr1,player *plr2);
 int player1_move(char **m,player *plr1, player *plr2);
 char border_boundaries(char **m,player *plr1, player *plr2,char move);
-#endif // FUNCTIONS_H_INCLUDED
+
